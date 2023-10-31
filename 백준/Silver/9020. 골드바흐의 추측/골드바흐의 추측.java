e34830a81bc310581608ea1dcddf11adea2d9ecb
조용준 <127725768+turn-world@users.noreply.github.com>
2023-10-31 21:56:42 +0900

    Update 골드바흐의 추측.java
    Plus Description

--- 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.java
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.java
@@ -7,12 +7,20 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        //BufferedReader BufferedWriter
+        //InputStreamReader 문자(열이 아님)를 입력 그대로 받아들이게 해줌 10진수 utf-16으로 받아들이게 되면 한글이 깨짐
+        //그래서 buffer에 한꺼번에 담아서 처리하는 거임
+        // BufferedWriter는 'write()', 'flush()', 'close()'
+        // write로 쓰고, flush로 담은 내용 출력 close로 닫음
         StringBuilder sb = new StringBuilder();
-
+        //문자열끼리 더하기는 매우 비효율적임
+        //StringBuilder는 변경가능한 문자열을 만들어줌
+        // 이때 append를 써서 추가해주면 됨
         get_prime();
 
         int T  = Integer.parseInt(in.readLine());
-
+        // Integer의 parseInt는 String타입의 숫자를 int타입으로 변환해주는 녀석
+        // parseInt는 Integer의 메소드임
         while (T-- > 0){
             int a = Integer.parseInt(in.readLine());
             int first_partion = a/2;
