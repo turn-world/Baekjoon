8e91650c58d0595ac4da006dc36ff896c580f133
조용준 <127725768+turn-world@users.noreply.github.com>
2023-11-10 01:31:59 +0900

    [Silver II] Title: K번째 소수, Time: 228 ms, Memory: 42032 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/15965. K번째 소수/K번째 소수.java
+++ 백준/Silver/15965. K번째 소수/K번째 소수.java
@@ -1,24 +1,25 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
-    public static boolean [] prime = new boolean[500001];
+    public static boolean [] prime = new boolean[8000002];
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         int k = Integer.parseInt(in.readLine());
+        List<Long> list = new ArrayList<Long>();
         chae();
-        int count =1;
-        for ( int i = 2; k <= 500000 ; i ++){
-            if(!prime[i]){
-                if(count == k){
-                    System.out.println(i);
-                    break;
-                }
-                count++;
+        for (long i = 2; i < 8000000; i++) {
+            //소수가 int자료형을 넘을 수 있으므로 long으로
+            if (!prime[(int) i]) {
+                list.add(i);
             }
         }
+
+        System.out.println(list.get(k - 1));
     }
     public static void chae(){
         prime[0] = prime[1] = true;
