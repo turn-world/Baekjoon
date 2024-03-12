a4a2015d27ef304e7aaa67f1f5d589a84f5c9986
조용준 <127725768+turn-world@users.noreply.github.com>
2024-03-12 15:12:24 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 348 ms, Memory: 27352 KB -BaekjoonHub

---
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int [][][]dp = new int[21][21][21];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //동적계획법의 특징은 대체로 재귀 + Memoization(메모이제이션)
+
+        StringBuilder sb = new StringBuilder();
+
+        while (true){
+            StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            if(a==-1 && b == -1 && c== -1){
+                break;
+            }
+
+            sb.append("w(" + a + ", " + b + ", " + c + ") = ").append(w(a,b,c)).append('\n');
+        }
+        System.out.println(sb);
+    }
+
+    static int w (int a, int b, int c){
+        //a,b,c가 범위를 벗어나지 않으면서 메모제이션이 되어 있는 경우
+        if(inRange(a,b,c) && dp[a][b][c] != 0 ){
+            return dp[a][b][c];
+        }
+        if(a<=0 || b<=0 || c<= 0 ){
+            return 1;
+        }
+        if( a> 20 || b > 20 || c> 20){
+            return dp[20][20][20] = w(20,20,20);
+        }
+        if(a < b && b < c){
+            return dp[a][b][c] = w(a,b,c-1) + w(a,b-1,c-1) - w(a,b-1,c);
+        }
+        return dp[a][b][c] = w(a-1,b,c) + w(a-1,b-1,c)+w(a-1,b,c-1) - w(a-1,b-1,c-1);
+    }
+
+    static boolean inRange(int a,int b, int c){
+        return 0<= a && a<= 20 && 0 <= b && b <=20 && 0<= c && c<=20;
+    }
+}
