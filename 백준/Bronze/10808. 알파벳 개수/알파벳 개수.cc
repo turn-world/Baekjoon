f362ecb6aa2424f396e180b271c3f2c24ca92446
조용준 <127725768+turn-world@users.noreply.github.com>
2024-08-21 10:45:48 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+ 
+int main() {
+	string S; // 단어 S
+	cin >> S; 
+ 
+	int alp[26] = { 0, }; // 알파벳 개수 담는 배열
+ 
+	for (int i = 0; i < S.length(); i++)
+	{
+		alp[S[i] - 'a'] +=1;
+	}
+ 
+	for (int i = 0; i < 26; i++)
+	{
+		cout << alp[i] << ' ';
+	}
+	
+	return 0;
+}
